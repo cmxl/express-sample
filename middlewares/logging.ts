5bb81be08577af0640780ef554a68bb841954d94
cmxl <th.cmxl@gmail.com>
2023-11-11 00:10:01 +0100

    log full path

--- middlewares/logging.ts
+++ middlewares/logging.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 
 export const logging = (req: Request, res: Response, next: NextFunction) => {
-	res.addListener('finish', () => console.log(`${req.method} ${req.path} => ${res.statusCode} ${res.statusMessage} ${res.get('Content-Length')}`));
+	res.addListener('finish', () => console.log(`${req.method} ${req.originalUrl} => ${res.statusCode} ${res.statusMessage} ${res.get('Content-Length') || ''}`));
 	next();
 };
